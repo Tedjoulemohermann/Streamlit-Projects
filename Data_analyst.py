4c1b5eea34b5dd98d799f2bf88104da232a49fcd
Hermann tedjoulemo <tedjoulemongueudehermann@gmail.com>
2024-10-24 00:09:22 +0100

    version 1

--- Data_analyst.py
+++ Data_analyst.py
@@ -4,8 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+sns.set(style='white', context='notebook', # Styles de Seaborn
+        palette=['#ff5252', '#1de9b6'])
+
 def main():
-    st.title("Hello, World! EDA Streamlit App")
+    st.title("Hi, l'm Hermann ! welcome to my EDA Streamlit Application")
 
     st.header("Upload your CSV data file")
     data_file = st.file_uploader("Upload CSV", type=["csv"])
@@ -15,9 +18,20 @@ def main():
         st.write("Data overview:")
         st.write(data.head())
 
-        st.sidebar.header("Visualizations")
-        plot_options = ["Bar plot", "Scatter plot", "Histogram", "Box plot"]
+        st.sidebar.header("Choose the visualization type")
+        plot_options = ["Bar plot", "Scatter plot", "Histogram", "Box plot","Line plot"]
         selected_plot = st.sidebar.selectbox("Choose a plot type", plot_options)
+
+        st.write("Information on your data (Shape and type of columns) :")
+        st.write(data.shape)
+        st.write(data.dtypes)
+
+
+        st.write("descriptive statistics of your data :")
+        st.write(data.describe())
+
+        st.write("Missing Values : ")
+        st.write(data.isna().sum())
 
         if selected_plot == "Bar plot":
             x_axis = st.sidebar.selectbox("Select x-axis", data.columns)
@@ -26,7 +40,13 @@ def main():
             fig, ax = plt.subplots()
             sns.barplot(x=data[x_axis], y=data[y_axis], ax=ax)
             st.pyplot(fig)
-
+        if selected_plot == "Line plot":
+            x_axis = st.sidebar.selectbox("Select x-axis", data.columns)
+            y_axis = st.sidebar.selectbox("Select y-axis", data.columns)
+            st.write("Line plot :")
+            fig, ax = plt.subplots()
+            sns.lineplot(x=data[x_axis], y=data[y_axis], ax=ax)
+            st.pyplot(fig)
         elif selected_plot == "Scatter plot":
             x_axis = st.sidebar.selectbox("Select x-axis", data.columns)
             y_axis = st.sidebar.selectbox("Select y-axis", data.columns)
