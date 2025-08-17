91d580a8963b1de0e445cd3f76fa31c96d680aef
Hermann tedjoulemo <tedjoulemongueudehermann@gmail.com>
2025-08-17 11:50:01 +0100

    version 1

---
+++ Blood_Donation_campaign_Challenge.py
@@ -0,0 +1,40 @@
+# Import librairies
+import streamlit as st
+import pandas as pd
+import altair as alt
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+# Page Configuration
+st.set_page_config(
+    page_title="Blood Donation Campaign Dashboard",
+    page_icon="🩸",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+#alt.themes.enable("dark")
+sns.set(style='dark', context='notebook',
+        palette=['#ff5252', '#1de9b6'])
+
+data = pd.read_excel("Updated Challenge dataset.xlsx")
+
+# Add a Sidebar
+with st.sidebar:
+    st.title('Blood Donation Campaign Dashboard')
+    sex_options=["Male", "Female"]
+    sex_plot = st.sidebar.selectbox("Sex",sex_options)
+    sitmat_options = ["Marié", "Célibataire"]
+    sex_plot = st.sidebar.selectbox("Situation matrimoniale", sitmat_options)
+
+st.title('Blood Donation Campaign Dashboard')
+##plot_genre=sns.countplot(data=data, x="Genre")
+#st.pyplot(plot_genre.get_figure())
+#plot_eli=sns.countplot(data=data, y="ÉLIGIBILITÉ AU DON.")
+#st.pyplot(plot_eli.get_figure())
+
+fig, ax =plt.subplots(3,1)
+sns.countplot(data=data,x="Genre", ax=ax[0])
+sns.countplot(data=data,x="ÉLIGIBILITÉ AU DON.",ax=ax[1])
+sns.histplot(data=data,x="Poids",ax=ax[2])
+st.pyplot(fig)
